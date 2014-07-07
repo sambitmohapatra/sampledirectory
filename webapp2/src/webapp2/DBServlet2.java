dc3ae3e2d740d8b19faedbcbdfcd45457916dab6
sambitmohapatra <sambit.mohapatra2007@gmail.com>
2014-07-07 16:49:47 +0530

    ok

---
+++ webapp2/src/webapp2/DBServlet2.java
@@ -0,0 +1,58 @@
+package webapp2;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * Servlet implementation class DBServlet
+ */
+@WebServlet("/DBServlet")
+public class DBServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public DBServlet2() {
+        super();
+    }
+    Session ses=null;
+	public void init()
+	{
+		Configuration conf = new Configuration().configure("/hibernate.cfg.xml");
+
+ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+
+SessionFactory sf = conf.buildSessionFactory(sr);
+
+ses = sf.openSession();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int no=Integer.parseInt(request.getParameter("teno"));
+		EmpBean eb=(EmpBean)ses.get(EmpBean.class,no);
+		request.setAttribute("res",eb);
+		RequestDispatcher rd=request.getRequestDispatcher("/result.jsp");
+		rd.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+	public void destroy()
+	{
+		ses.close();
+	}
+
+}
